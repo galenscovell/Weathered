1cefefe404669cd19ccce32aa82d52659bea9706
Galen Scovell <galen.scovell@gmail.com>
2014-12-08 14:49:25 -0800

    JSON Parsing Setup
    Basic functionality mostly complete, now the question is how far I ought
    to take this

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require 'sinatra'
 require 'sinatra/flash'
 require 'json'
 require 'open-uri'
-require_relative 'models'
 
 
 configure do
@@ -20,17 +19,29 @@ get '/' do
 end
 
 post '/enter' do
-  @location = params[:location]
+  @location = (params[:location]).split(' ').join(',')
   site_url = "http://api.openweathermap.org/data/2.5/weather?q=" + @location
   jhash = JSON.parse(open(site_url).read)
-  @titles = []
-  @infos = []
 
-  jhash['main'].each do |w|
-    title_tag = w[0]
-    info_item = w[1]
-    @titles << "#{title_tag}"
-    @infos << "#{info_item}"
+  if jhash['cod'] != '404'
+    @name = jhash['name'].capitalize
+    @country = jhash['sys']['country']
+    @coords = jhash['coord']['lon'].to_s + ", " + jhash['coord']['lat'].to_s
+
+    @description = []
+    jhash['weather'].each do |type|
+      @description << type['description']
+    end
+
+    @temp = jhash['main']['temp'].to_i
+    @celsius = (@temp - 273.15).round(2)
+    @fahr = ((@celsius * 1.8) + 32).round(2)
+    @humidity = jhash['main']['humidity']
+    @pressure = jhash['main']['pressure']
+    @wind = jhash['wind']['speed']
+    @wind_direction = jhash['wind']['deg']
+  else
+    redirect '/', flash[:error] = "Location not found."
   end
 
   erb :index
